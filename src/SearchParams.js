5909fda95ea8694ccfcf153a919efdde7fcf1e3f
Viorel <viorel_popa89@yahoo.com>
2022-05-06 10:41:09 +0300

    add animals options to form

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  //const location = "Seattle, WA";
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+
   return (
     <div className="search-params">
       <form>
@@ -14,6 +17,30 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+              return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>

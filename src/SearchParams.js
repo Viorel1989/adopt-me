134be2a64e050afb74b4da3d800ceaa093744205
Viorel <viorel_popa89@yahoo.com>
2022-06-02 08:53:25 +0300

    refactor result component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import useBreedList from "./useBreedList";
-import Pet from "./Pet";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -8,9 +8,7 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-
   const [breeds] = useBreedList(animal);
-
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
@@ -87,14 +85,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };

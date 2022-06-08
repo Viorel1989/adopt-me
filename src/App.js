5a99b45f33a729dc6a06899c0fdccc680c677859
Viorel <viorel_popa89@yahoo.com>
2022-06-08 14:03:52 +0300

    add Context to toggle button colors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 import Details from "./Details";
 import { StrictMode } from "react";
+import { useState } from "react";
 
 const App = () => {
+  const theme = useState("blue");
   return (
     <StrictMode>
-      <BrowserRouter>
-        <header>
-          <Link to="/">Adopt me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />}></Route>
-          <Route path="/" element={<SearchParams />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />}></Route>
+            <Route path="/" element={<SearchParams />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
